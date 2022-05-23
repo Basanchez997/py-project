e07765abff46fa0e255a3549eb00b2fc15718c58
Basanchez997 <105731819+Basanchez997@users.noreply.github.com>
2022-05-23 08:04:05 -0500

    Pc

---
+++ convul.py
@@ -0,0 +1,8 @@
+#--------------Importar librerias
+import os
+import tensorflow as tf #linreria de IA
+import cv2 #Lib de opencv
+import matplotlib.pyplot as plt #Lib de observar imagenes
+import numpy as np #Operaciones
+from tensorflow.keras.callbacks import TensorBoard #Lb para observar el funcionamineto de l ared 
+from tensorflow.keras.preprocessing.image import ImageDataGenerator #lib para modificar mis img
