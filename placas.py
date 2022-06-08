f8110d446734192d24a65a331f336c3d6f41b311
Basanchez997 <105731819+Basanchez997@users.noreply.github.com>
2022-06-07 21:40:39 -0500

    Agregando funion
    Funion de cada 5 seg

--- placas.py
+++ placas.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np #Operaciones
 import pytesseract
 from PIL import Image
+import time
 
 #Video captura
 #video="http://192.168.0.107:31522/videostream.cgi?user=admin&pwd=888888" 
@@ -13,70 +14,15 @@ cap = cv2.VideoCapture(video)
 
 Ctexto = ''
 
-
-#Creamos nuestro while true
-while True:
-#Realizamos la lectura de la videoCaptura
-    ret, frame = cap.read()
-    al1, an1, c1 = frame.shape
-    redmin = cv2.resize(frame, (an1*2, al1*2))
-    if ret == False:
-        break
-
-    #Dibujamos un rectangulo
-    cv2.rectangle(redmin, (870, 750), (1070, 850), cv2.FILLED)
-    cv2.putText(redmin, Ctexto[0:7], (900, 810), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-    #eXTRAEMOS EL ANCHO Y EL ALTO DE LOS FOTOGRAMAS
-    al, an, c = redmin.shape
-    #Tomar el cntro de la imagen en X:
-    x1 = int(an / 2) # tomamos el 1/3 de la imagen
-    x2 = int(x1 * 2 ) # Hasta el inicio del 3/3 de la imagen
-
-    #En Y:
-    y1 = int(al / 2) # tomamos el 1/3 de la imagen
-    y2 = int(y1 * 2 ) # Hasta el inicio del 3/3 de la imagen
-
-    #Texto
-    cv2.rectangle(redmin, (x1 + 160, y1 + 500), (300, 600), (0, 0, 0), cv2.FILLED)
-    cv2.putText(redmin, 'PROCESANDO PLACOTA ', (x1 + 180, y1 + 550), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-    #uBICAMOS EL RECTANGULO EN LA ZONAS EXTRAIDAS
-    cv2.rectangle(redmin, (x1, y1), (x2, y2), (0, 255, 0), 2)
-
-    #Realizamos un recorte a nuestra zona de interes
-    recorte = redmin[y1:y2, x1:x2]
-    #prueba
-    recorte2 = cv2.cvtColor(recorte, cv2.COLOR_BGR2GRAY)
-    #Reprocesamiento de la zona de interes
-    mB = np.matrix(recorte[:, :, 0])
-    mG = np.matrix(recorte[:, :, 1])
-    mR = np.matrix(recorte[:, :, 2])
-
-    #restamos el color verde y azul para sacar amarillo
-    Color = cv2.absdiff(mB, mR)
-    Color = cv2.absdiff(mB, mR, mG)
-    #cv2.imshow("Test", recorte)}
-    #prueba
-    _, umbral = cv2.threshold(Color, 150, 255, cv2.THRESH_BINARY)
-    #_, umbral = cv2.threshold(recorte2, 180, 200, cv2.THRESH_BINARY)
-    #cv2.imshow("Antes", umbral)
-    #prueba = cv2.blur(umbral,(3,3))
-    #umbral_limpio = cv2.Canny(umbral, 150, 200)
-    umbral_limpio = cv2.dilate(umbral, None, iterations=1)
-    #cv2.imshow("Test", umbral_limpio)
-    #Extraemos los contornos de la zona seleccionada
-    contornos, _ = cv2.findContours(umbral_limpio, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    
-    #Primero los ordenamos del mas grande al mas pequeño
-    contornos = sorted(contornos, key=lambda x : cv2.contourArea(x), reverse=True)
-    
+def funcion_final(contornos = None):
+    if contornos == None:
+        print("Error, debes enviar dos números a la función")
+        return
     #Dibujamos los contornos extraidos
     for contorno in contornos:
         area = cv2.contourArea(contorno)
-        #print("areaaa", area)
-        if area > 15000:
-            #cv2.imshow("Test", area)
+        print(area)
+        if area > 10000:
             #Detectamos la placa
             x, y, ancho, alto = cv2.boundingRect(contorno)
 
@@ -112,22 +58,16 @@ while True:
             #estamos el color verde y azul para sacar amarillo
             Color2 = cv2.absdiff(mBp, mRp)
             Color2 = cv2.absdiff(mBp, mRp, mGp)
-            #cv2.imshow("nose", Color2)
             #Binarizamos l aimagen
-            #cv2.imshow("Test", recorte)
             _, umbral2 = cv2.threshold(Color2, 150, 255, cv2.THRESH_BINARY)
-            #cv2.imshow("Antes", umbral)
-            #prueba = cv2.blur(umbral,(3,3))
-            #umbral_limpio2 = cv2.Canny(umbral2, 150, 200)
             umbral_limpio2 = cv2.dilate(umbral2, None, iterations=1)
-            #cv2.imshow("Test", umbral_limpio)
             #for col in range(0, alp):
              #   for fil in range(0, anp):
               #      Max = max(mRp[col,fil], mGp[col,fil], mBp[col, fil])
                #     Mva[col, fil] = 255 - Max
                 #    print("Max",Max)
                  #   cv2.imshow("Mva", Mva)
-            #cv2.imshow("umbral_limpio2", umbral_limpio2)
+            cv2.imshow("umbral_limpio2", umbral_limpio2)
             #Binarizamos la imagen
             _, bin = cv2.threshold(umbral_limpio2, 150, 255, cv2.THRESH_BINARY)
 
@@ -136,7 +76,7 @@ while True:
             bin = Image.fromarray(bin)
             bin = bin.convert("L")
             #Nos aseguramos de tener un buen tamaño de la placa
-            if alp >= 50 and anp >= 100:
+            if alp >= 100 and anp >= 200:
                 #Declaramos la direccion de Pyresseract
                 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
                 #Extraemos el texto
@@ -149,13 +89,68 @@ while True:
         
         
         
-            
+            time.sleep(10)
             break
+    
+
+
+#Creamos nuestro while true
+while True:
+#Realizamos la lectura de la videoCaptura
+    ret, frame = cap.read()
+    al1, an1, c1 = frame.shape
+    redmin = cv2.resize(frame, (an1+1, al1+1))
+    if ret == False:
+        break
+
+    #Dibujamos un rectangulo
+    cv2.rectangle(frame, (870, 750), (1070, 850), cv2.FILLED)
+    cv2.putText(frame, Ctexto[0:7], (900, 810), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+    #eXTRAEMOS EL ANCHO Y EL ALTO DE LOS FOTOGRAMAS
+    al, an, c = redmin.shape
+    #Tomar el cntro de la imagen en X:
+    x1 = int(an / 2) # tomamos el 1/3 de la imagen
+    x2 = int(x1 * 2 ) # Hasta el inicio del 3/3 de la imagen
+
+    #En Y:
+    y1 = int(al / 2) # tomamos el 1/3 de la imagen
+    y2 = int(y1 * 2 ) # Hasta el inicio del 3/3 de la imagen
+
+    #Texto
+    cv2.rectangle(frame, (x1 + 160, y1 + 500), (300, 600), (0, 0, 0), cv2.FILLED)
+    cv2.putText(frame, 'PROCESANDO PLACOTA ', (x1 + 180, y1 + 550), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+    #uBICAMOS EL RECTANGULO EN LA ZONAS EXTRAIDAS
+    cv2.rectangle(redmin, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+    #Realizamos un recorte a nuestra zona de interes
+    recorte = redmin[y1:y2, x1:x2]
+    #prueba
+    recorte2 = cv2.cvtColor(recorte, cv2.COLOR_BGR2GRAY)
+    #Reprocesamiento de la zona de interes
+    mB = np.matrix(recorte[:, :, 0])
+    mG = np.matrix(recorte[:, :, 1])
+    mR = np.matrix(recorte[:, :, 2])
+
+    #restamos el color verde y azul para sacar amarillo
+    Color = cv2.absdiff(mB, mR)
+    Color = cv2.absdiff(mB, mR, mG)
+    _, umbral = cv2.threshold(Color, 150, 255, cv2.THRESH_BINARY)
+    #_, umbral = cv2.threshold(recorte2, 180, 200, cv2.THRESH_BINARY)
+    umbral_limpio = cv2.dilate(umbral, None, iterations=1)
+    #Extraemos los contornos de la zona seleccionada
+    contornos, _ = cv2.findContours(umbral_limpio, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    
+    #Primero los ordenamos del mas grande al mas pequeño
+    contornos = sorted(contornos, key=lambda x : cv2.contourArea(x), reverse=True)
+     #aqui iria la funcion
+    funcion_final(contornos)
             
     #Mostramos el reporte en gris
-    #cv2.imshow("Vehiculos", frame)
+    cv2.imshow("frame", frame)
     
-    #cv2.imshow("Vehiculos2", redmin)
+    cv2.imshow("redmin", redmin)
 
     #Leemos una tecla
     t = cv2.waitKey(1)
@@ -165,4 +160,5 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
 
